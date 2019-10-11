41b924eafbc6b90ce3ff7a7c85e44f94054201d7
hanichu <hendrymino@gmail.com>
2019-10-11 17:18:59 +0200

    added services and messages  component&service

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -1,13 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
 
-describe('HeroService', () => {
+describe('MessageService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: HeroService = TestBed.get(HeroService);
+    const service: MessageService = TestBed.get(MessageService);
     expect(service).toBeTruthy();
   });
 });
-
